435cb89491984bb4dd1275f7892b7ecb332f3b35
adrian <adrian.rojas.o@gmail.com>
2025-09-19 11:31:51 -0400

    port configuration

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
-console.log("Hello World");
+//for the app to start
+try{
+    //listening port, if there is not a var in .env, the port is 3000
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log('Server active on port: '+ PORT));
+}catch(e){
+    console.log(e);
+}
